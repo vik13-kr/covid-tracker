830427b8f43da2fad30ebc18007d57a9ea93f835
Vikash <13vik.kr@gmail.com>
2021-04-11 01:59:45 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+/** @format */
+
+import "./App.css";
+import Cards from "./components/Cards/Cards";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="app">
+			<div className="header">
+				<div className="page_title">
+					<h2>Covid 19 Tracker</h2>
+				</div>
+				<div className="country_selector">Select</div>
+			</div>
+			<div className="body_container">
+				<div className="left_container">
+					<div className="cards">
+						<Cards />
+						<Cards />
+						<Cards />
+					</div>
+					<div className="map"></div>
+				</div>
+				<div className="right_container">Hey </div>
+			</div>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/components/Cards/Cards.js
@@ -0,0 +1,16 @@
+/** @format */
+
+import React from "react";
+import "./Cards.css";
+
+function Cards() {
+	return (
+		<div className="container">
+			<div className="title"> Cases</div>
+			<div className="value">5475545</div>
+			<div className="total_value">557448856 Total </div>
+		</div>
+	);
+}
+
+export default Cards;
